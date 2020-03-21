2a0b3724c22a83b3b513f27d89cd16dbffe4febb
pitca8413 <pitca8413@gmail.com>
2020-03-21 15:36:05 +0900

    sklearn 데이터 이미지 셋 출력하기

---
+++ digitImage02.py
@@ -0,0 +1,15 @@
+# 이미지 여러개 출력해보기
+# digitImage02.py
+
+import sklearn.datasets
+import matplotlib.pyplot as plt
+
+digits = sklearn.datasets.load_digits()
+
+for idx in range(50):
+    plt.subplot(5, 10, idx + 1)
+    plt.axis("off")
+    plt.title(digits.target[idx])
+    plt.imshow(digits.images[idx], cmap='Greys')
+
+plt.show()
